d7594f2817d3e6ce171100c1dc5f029d602756c1
Muhammad-Tabish-123 <tabish.baqa@gmail.comm>
2022-03-10 20:49:41 +0500

    Whatsapp-web

---
+++ forIframe.js
@@ -0,0 +1,64 @@
+function Iframe(){
+    document.getElementById('Defaultimg').style.display="none";
+    document.getElementById('Iframe').style.display="block";
+    
+}
+function cls(){
+    document.getElementById('contact-name').style.display="none";
+//    document.getElementsByTagName('table').style.display="none";
+}
+
+
+
+//select nth child method
+// document.querySelector('td:nth-child(1)').style.display="none";
+// document.querySelector('tr:nth-child(2)').style.display="none";
+// document.querySelector('tr:nth-child(3)').style.display="none";
+// document.querySelector('tr:nth-child(4)').style.display="none";
+
+
+// const One= document.querySelector('td:nth-child(1)') ;
+// console.log(One);
+// const Two= document.querySelector('td:nth-child(2)') ;
+// console.log(Two);
+// const third= document.querySelector('tr:nth-child(3)') ;
+// console.log(third);
+// const Four= document.querySelector('tr:nth-child(4)') ;
+// console.log(Four);
+
+// const array_of_rows=document.getElementsByTagName("<tr>");
+
+
+
+
+//Bilal array method test
+// document.getElementById('Defaultimg').style.height="100%";
+// document.getElementById('Defaultimg').style.width="100%";
+// document.querySelector('td:nth-child(1)').style.display="none";
+// const array_of_rows=document.getElementsByTagName("tr");
+// console.log(array_of_rows);
+
+// for (let i = 1; i < 4; i++) {
+//     array_of_rows[i].style.display="none";
+//   }
+
+
+
+//screen width with f condition
+// if (screen.width < 960) {
+//     alert('Less than 960');
+// }
+// else {
+
+//     alert('More than 960');
+// }
+
+// for(let i=0;i<1;){
+//     if (screen.width < 960) {
+//         alert('Less than 960');
+//     }
+//     else {
+    
+//         alert('More than 960');
+//     }
+// }
