6ec91dbf24889b0356f3c3d9f904a4cd782d2de6
Muhammad-Tabish-123 <tabish.baqa@gmail.comm>
2022-03-13 13:31:15 +0500

    Attachment button

---
+++ JavaScript/Chatbox.js
@@ -0,0 +1,10 @@
+function File_Attachment(){
+    // if(document.getElementById('footer-file-attachment-options').style.display="block"){
+    //     document.getElementById('footer-file-attachment-options').style.display="none";
+    //   }
+    if(document.getElementById('footer-file-attachment-options').style.display = "none"){
+    document.getElementById('footer-file-attachment-options').style.display="block";
+} else if(document.getElementById('footer-file-attachment-options').style.display="block"){ 
+   document.getElementById('footer-file-attachment-options').style.display="none";
+}
+}
